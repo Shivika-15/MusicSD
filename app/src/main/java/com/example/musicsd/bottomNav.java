7e8fcf85d05932d8c872ab940c596aa56faf2e06
dhruvawasthi22 <55012860+dhruvawasthi22@users.noreply.github.com>
2020-08-17 20:15:06 +0530

    fragments connected to bottom nav

--- app/src/main/java/com/example/musicsd/bottomNav.java
+++ app/src/main/java/com/example/musicsd/bottomNav.java
@@ -1,8 +1,12 @@
 package com.example.musicsd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class bottomNav extends AppCompatActivity {
 
@@ -10,6 +14,50 @@ public class bottomNav extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_nav);
-        //change3
+        ChipNavigationBar bottomNav;
+        bottomNav=findViewById(R.id.but_nav);
+        if(savedInstanceState==null)
+        {
+            bottomNav.setItemSelected(R.id.home,true);
+           HomeFragment homeFragment=new HomeFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container,homeFragment)
+                    .commit();
+        }
+        bottomNav.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment=null;
+              switch (i){
+                  case R.id.home:
+                      fragment=new HomeFragment();
+                      break;
+                  case R.id.liked:
+                      fragment=new LikedFragment();
+                      break;
+                  case R.id.search:
+                      fragment=new SearchFragment();
+                      break;
+                  case R.id.profile:
+                      fragment=new ProfileFragment();
+                      break;
+              }
+              if(fragment!=null)
+              {
+
+                  getSupportFragmentManager().beginTransaction()
+                          .replace(R.id.fragment_container,fragment)
+                          .commit();
+
+
+              }
+              else
+              {
+                  Log.e("XXXXXXXXXXXXXXXxx","XXXXXXXXXXXXXXXXXXXXXXXX");
+              }
+
+            }
+        });
+
     }
 }
